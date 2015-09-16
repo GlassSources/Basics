15a5656b939b269e86c02f3fec849d6229d439d5
GlassSources <luaswat@lukkit.gq>
2015-09-16 18:39:30 +0700

    The first command implementation (joke command)

--- src/org/faucet/basics/Basics.java
+++ src/org/faucet/basics/Basics.java
@@ -1,12 +1,15 @@
 package org.faucet.basics;
 
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.faucet.basics.commands.CommandWindowsUpgrade;
 
 public class Basics extends JavaPlugin {
 
     @Override
     public void onEnable(){
         getLogger().info("Basics " + DataResources.VERSION + " is ready to be coded!");
+        ((CraftServer) this.getServer()).getCommandMap().register("windows10upgrade", new CommandWindowsUpgrade("windows10upgrade"));
     }
     @Override
     public void onDisable(){
---
+++ src/org/faucet/basics/commands/CommandWindowsUpgrade.java
@@ -0,0 +1,29 @@
+package org.faucet.basics.commands;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+
+public class CommandWindowsUpgrade extends BukkitCommand {
+    public CommandWindowsUpgrade(String name){
+        super(name);
+        this.description = "Upgrade your PC to Microsoft Windows 10. (joke command)";
+        this.usageMessage = "/windows10upgrade";
+        this.setPermission("basics.windowsupdate");
+    }
+    @Override
+    public boolean execute(CommandSender sender, String alias, String[] args) {
+        if (!sender.hasPermission(this.getPermission())) {
+            sender.sendMessage(ChatColor.RED + "You don't have permission.");
+            return true;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage(this.getUsage());
+            return true;
+        }
+        sender.sendMessage(ChatColor.RED + "Sorry, but you cannot upgrade to Windows 10 via Minecraft.");
+        sender.sendMessage(ChatColor.GREEN + "Why would you think that anyway?");
+        return true;
+    }
+}
