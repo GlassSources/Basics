44a62e52e50c785b3ccec55b16b5ed25f4fa1f7f
GlassSources <luaswat@lukkit.gq>
2015-09-17 20:41:06 +0700

    Final Implementation of Fly Command

--- src/org/faucet/basics/Basics.java
+++ src/org/faucet/basics/Basics.java
@@ -2,6 +2,7 @@ package org.faucet.basics;
 
 import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.faucet.basics.commands.CommandFly;
 import org.faucet.basics.commands.CommandSpeed;
 import org.faucet.basics.commands.CommandWindowsUpgrade;
 
@@ -12,6 +13,7 @@ public class Basics extends JavaPlugin {
         getLogger().info("Basics " + DataResources.VERSION + " is ready to be coded!");
         ((CraftServer) this.getServer()).getCommandMap().register("windows10upgrade", new CommandWindowsUpgrade("windows10upgrade"));
         ((CraftServer) this.getServer()).getCommandMap().register("speed", new CommandSpeed("speed"));
+        ((CraftServer) this.getServer()).getCommandMap().register("fly", new CommandFly("fly"));
     }
     @Override
     public void onDisable(){
