d066e4ffe9f933b402a96cb1521f15e432be0091
GlassSources <luaswat@lukkit.gq>
2015-09-18 15:31:20 +0700

    2.1 Change to Plugin.YML and DataResources.

--- src/org/faucet/basics/DataResources.java
+++ src/org/faucet/basics/DataResources.java
@@ -1,7 +1,7 @@
 package org.faucet.basics;
 
 public class DataResources {
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.1";
     public static final String NAME = "Basics";
 
 }
